052420b20129f818ae6d25e8e22b069404529c9b
Mathieu Garrigues <m.garrigues@wildense.co>
2023-10-27 18:01:46 +0200

    Add name_features_by_count

--- src/mol/features.py
+++ src/mol/features.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from attr import dataclass
 from rdkit.Chem.rdMolChemicalFeatures import MolChemicalFeature
 
@@ -22,5 +24,33 @@ class Feature:
         self.__position = rd_feature.GetPos()
         self.__molecule_id = molecule_id
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name: str) -> None:
+        self.__name = new_name
+
+    @property
+    def family(self):
+        return self.__family
+
     def __print__(self):
         print(f"Feature {self.__name} - {self.__family} - Coordinated {self.__position}")
+
+
+def name_features_by_count(features: list[Feature], is_ligand: bool) -> None:
+    """Gives in-place a unique ordered name to each feature of the list, according to their type.
+
+    Args:
+        features (list[Feature]): List of features of different types
+        is_ligand (bool): If the features' molecule is a ligand, their name will be in upper case.
+    """
+    counter: Counter[int] = Counter()
+
+    for feature in features:
+        abbreviation = feature.family.abbreviation
+        ordered_id = counter[abbreviation]
+        counter[abbreviation] += 1
+        feature.name = abbreviation + str(ordered_id)
